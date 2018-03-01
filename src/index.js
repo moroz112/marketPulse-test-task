69198280ee2d093b6ca22eb6021d8569f508507a
amoroz <morozalexrpd@gmail.com>
2018-03-01 18:37:14 +0200

    test-task first commit

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import './main.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reducers from 'reducers';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension'
+import { browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Provider } from 'react-redux';
+import { Router, Route } from 'react-router'
+import Layout from 'containers/layout';
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)));
+
+const history = syncHistoryWithStore(browserHistory, store);
+ReactDOM.render(
+	<Provider store={store}>
+		<Router history={history}>
+			<Route path="/" component={Layout}></Route>
+		</Router>
+	</Provider>,
+	document.getElementById('root')
+);
